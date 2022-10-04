a5501308223dd2bce49b58989ae001d8e3b70408
gyuha <gyuha0310@gmail.com>
2022-10-04 18:11:38 +0900

    init

---
+++ src/components/navbar/NavbarContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "#",
+    current: true,
+  },
+  {
+    name: "Team",
+    href: "#",
+    current: true,
+  },
+  {
+    name: "Projects",
+    href: "#",
+    current: true,
+  },
+];
+
+const NavbarContainer = () => {
+  return (
+    <>
+      <div />
+    </>
+  );
+};
+
+export default NavbarContainer;
---
+++ src/template/view.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const View = () => {
+  return (
+    <>
+      <div />
+    </>
+  );
+};
+
+export default View;
